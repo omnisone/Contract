bf055da68e85b420d6335fc033635ef80f27a138
a-rothwell <a_rothwell@outlook.com>
2018-05-13 19:49:05 -0500

    Pause contract and withfraw balance

--- contracts/Song.sol
+++ contracts/Song.sol
@@ -9,7 +9,7 @@ contract Song is ERC721Token ("Song", "SNG"), Ownable {
     using SafeMath for uint64;
 
     uint coinCost = 1;
-
+    bool pause = false;
     struct SongTokenData {
         string name;
         string magnetLink;
@@ -21,22 +21,42 @@ contract Song is ERC721Token ("Song", "SNG"), Ownable {
     mapping(uint256 => SongTokenData) public songData;
 
     function createToken(string name, string link, address artist) private {
+        require(!pause);
         uint256 _tokenId = totalSupply().add(1);
         _mint(msg.sender, _tokenId);
         addSongData(name, link, artist, _tokenId);
     }
 
     function addSongData(string name, string link, address artist, uint256 _tokenId) private {
+        require(!pause);
         songData[_tokenId] = SongTokenData(name, link, artist, now, now);
     }
 
     function payMinting() external payable{
+        require(!pause);
         require(msg.value == coinCost);
-        createToken("Song1.1", "URL", 0x212);
+        createToken("Song1.1", "URL", owner);
     }
 
     function extendLease(uint _tokenId) external payable {
-        require(msg.value > 0);
+        require(!pause);
         songData[_tokenId].tokenExpire = songData[_tokenId].tokenExpire.add(msg.value * 36000000);
+    }
+
+    function withdraw() onlyOwner external{
+        require(address(this).balance > 0);
+        owner.transfer(address(this).balance);
+    }
+
+    function getContractBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+
+    function pauseContract() onlyOwner public {
+        pause = !pause;
+    }
+
+    function getPause() public view returns (bool) {
+        return pause;
     }
 }
